db1b2354acf06a28883a20e6010faafc59e5d469
deadly parkour killer dark brawl stars assassin stalker sniper 1998 rus <annagleb08@gmail.com>
2024-10-20 02:19:57 +0300

    Смена темы на странице входа и регистрации; Изменена структура таблицы Юзеры;

--- models.py
+++ models.py
@@ -11,9 +11,11 @@ class UserType(db.Model):
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    nickname = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+    grade = db.Column(db.String(80), nullable=False)
     password = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(80), unique=True, nullable=False)
     usertype_id = db.Column(db.Integer, db.ForeignKey('user_type.id'))
 
 
--- static/plane1.js
+++ static/plane1.js
@@ -1,23 +1,39 @@
 function plane1(){
-
     let f = document.body;
-    let MI = document.getElementById("mewing")
-    let E = document.getElementsByClassName("Войти")
-    document.getElementById("FedorTiLoh").style.visibility = "hidden"
+    let MI = document.getElementById("mewing");
+    let E = document.getElementsByClassName("authForm");
+    document.getElementById("FedorTiLoh").style.visibility = "hidden";
     let enterBtn = document.getElementsByClassName("Enter")[0];
-    let registerBtn = document.getElementsByClassName("Register")[0];
+    //let registerBtn = document.getElementsByClassName("Register")[0];
+    let btnList = $('.authForm').find('button');
+    let inputList = $('input');
     let themeBtn = document.getElementsByTagName("button")[0];
-    if (f.style.backgroundColor === 'rgb(127, 127, 127)') {
+
+    if (f.style.backgroundColor === 'rgb(127, 127, 127)') { // светлая тема
         f.style.backgroundColor = "white";
-        f.style.color = enterBtn.style.color = registerBtn.style.color = "black";
+        f.style.color = "black";
         MI.src = "static/Безымянный.png";
         E[0].style.backgroundColor = 'rgb(223, 223, 223)';
-        enterBtn.style.backgroundColor = registerBtn.style.backgroundColor = 'rgb(211, 211, 211)';
-    } else {
+        for (const item of btnList) {
+            item.style.backgroundColor = 'rgb(211, 211, 211)'
+        }
+        for (const item of inputList) {
+            item.style.backgroundColor = 'white';
+        }
+        if ($('select').length > 0)
+            $('select').css('backgroundColor', 'white');
+    } else { // тёмная тема
         f.style.backgroundColor = "rgb(127, 127, 127)"
-        f.style.color = enterBtn.style.color = registerBtn.style.color = "lightgray";
+        f.style.color = "lightgray";
         MI.src = "static/Тёмный безымянный.png"
         E[0].style.backgroundColor = 'rgb(100, 100, 100)'
-        enterBtn.style.backgroundColor = registerBtn.style.backgroundColor = 'grey';
+        for (const item of btnList) {
+            item.style.backgroundColor = 'grey';
+        }
+        for (const item of inputList) {
+            item.style.backgroundColor = 'darkgray';
+        }
+        if ($('select').length > 0)
+            $('select').css('backgroundColor', 'darkgray');
 }   }
 
